8d5afaabfbb5d68cf6d15c6608e0e2e2be5a23e4
morgan <pichat.morgan@gmail.com>
2021-10-21 10:36:36 +0200

    Init project

---
+++ src/main/java/com/example/SpringController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spring")
+public class SpringController {
+
+    @GetMapping
+    public String bonjour() {
+        return "Bonjour Spring";
+    }
+
+}
